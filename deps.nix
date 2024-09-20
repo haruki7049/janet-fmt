fff1500d383d73cf5c36d5e9867b7c2b8c4ad722
haruki7049 <tontonkirikiri@gmail.com>
2024-09-20 23:08:33 +0900

    chore(deps): Add spork to use argparse

--- deps.nix
+++ deps.nix
@@ -1,3 +1,13 @@
-{ }:
+{ fetchgit }:
 
-[ ]
+[
+  {
+    name = "spork";
+    src = fetchgit {
+      url = "https://github.com/janet-lang/spork";
+      rev = "4c77afc17eb5447a1ae06241478afe11f7db607d";
+      hash = "sha256-nYSCWX262nhWn9hfd+kqnkH8ydN+DYg/XbCmGkozYZM=";
+    };
+    deps = [ ];
+  }
+]
